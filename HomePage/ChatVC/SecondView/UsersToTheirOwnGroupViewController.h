1cf211e98226cfa744f168340f84c28bd3a741d0
消汇邦 <543437229@qq.com>
2016-11-07 20:13:08 +0800

    扫描群二维码

---
+++ HomePage/ChatVC/SecondView/UsersToTheirOwnGroupViewController.h
@@ -0,0 +1,13 @@
+//
+//  UsersToTheirOwnGroupViewController.h
+//  XiaoHuiBang
+//
+//  Created by 消汇邦 on 2016/11/7.
+//  Copyright © 2016年 消汇邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UsersToTheirOwnGroupViewController : UIViewController
+
+@end
---
+++ HomePage/ChatVC/SecondView/UsersToTheirOwnGroupViewController.m
@@ -0,0 +1,37 @@
+//
+//  UsersToTheirOwnGroupViewController.m
+//  XiaoHuiBang
+//
+//  Created by 消汇邦 on 2016/11/7.
+//  Copyright © 2016年 消汇邦. All rights reserved.
+//
+
+#import "UsersToTheirOwnGroupViewController.h"
+
+@interface UsersToTheirOwnGroupViewController ()
+
+@end
+
+@implementation UsersToTheirOwnGroupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
