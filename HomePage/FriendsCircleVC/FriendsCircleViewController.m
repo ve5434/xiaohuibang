b4a98085676c6a2c0e85974046f672baf758a7f9
CYC666 <13705038428@163.com>
2016-11-07 15:15:04 +0800

    添加了发送动态显示的alert，功能未实现

--- HomePage/FriendsCircleVC/FriendsCircleViewController.m
+++ HomePage/FriendsCircleVC/FriendsCircleViewController.m
@@ -7,6 +7,10 @@
 //
 
 #import "FriendsCircleViewController.h"
+#import "MomentView.h"
+
+#define kScreenHeight [UIScreen mainScreen].bounds.size.height  // 屏高
+#define kScreenWidth [UIScreen mainScreen].bounds.size.width    // 屏宽
 
 @interface FriendsCircleViewController ()
 
@@ -25,7 +29,126 @@
     title.textAlignment = NSTextAlignmentCenter;
     title.textColor = [UIColor whiteColor];
     self.navigationItem.titleView = title;
+    
+    // 发布动态按钮
+    UIBarButtonItem *sendMomentsButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"icon_camera"]
+                                                                           style:UIBarButtonItemStylePlain
+                                                                          target:self
+                                                                          action:@selector(sendMomentsAction:)];
+    [self.navigationItem setRightBarButtonItem:sendMomentsButton];
+    self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    
+    
 }
+
+#pragma mark - 发布动态
+- (void)sendMomentsAction:(UIBarButtonItem *)button {
+    
+    UIView *virtualView = [[UIView alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    virtualView.backgroundColor = [UIColor colorWithWhite:0 alpha:.3];
+    [self.view.window addSubview:virtualView];
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapAction:)];
+    [virtualView addGestureRecognizer:tap];
+    
+
+    // 创建选项view，提供功能选着
+    UIView *alertView = [[UIView alloc] initWithFrame:CGRectMake(0,kScreenHeight , kScreenWidth, kScreenHeight*.3)];
+    alertView.backgroundColor = [UIColor colorWithRed:229/255.0 green:229/255.0 blue:229/255.0 alpha:1];
+    alertView.tag = 1001;
+    [virtualView addSubview:alertView];
+    
+    [UIView animateWithDuration:.35
+                     animations:^{
+                         alertView.transform = CGAffineTransformMakeTranslation(0, -kScreenHeight*.3);
+                     }];
+    
+    float buttonHeight = (kScreenHeight*.3 - 10)/4.0;
+    NSArray *titleArr = @[@"记录生活", @"视频拍照", @"从手机相册选择", @"取消"];
+    for (int i = 0; i < 4; i++) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+        button.titleLabel.font = [UIFont systemFontOfSize:15];
+        button.tag = 1500 + i;
+        [button setTitle:titleArr[i] forState:UIControlStateNormal];
+        [button setBackgroundColor:[UIColor whiteColor]];
+        [button setTitleColor:[UIColor colorWithRed:51/255.0 green:51/255.0 blue:51/255.0 alpha:1]
+                     forState:UIControlStateNormal];
+        button.layer.borderWidth = 0.5;
+        button.layer.borderColor = [UIColor colorWithRed:238/255.0 green:238/255.0 blue:238/255.0 alpha:1].CGColor;
+        if (i == 0) {
+            button.frame = CGRectMake(0, 0, kScreenWidth, buttonHeight);
+        } else if (i == 1) {
+            button.frame = CGRectMake(0, buttonHeight + 1, kScreenWidth, buttonHeight);
+        } else if (i == 2) {
+            button.frame = CGRectMake(0, buttonHeight*2 + 2, kScreenWidth, buttonHeight);
+        } else {
+            button.frame = CGRectMake(0, kScreenHeight*.3 - buttonHeight, kScreenWidth, buttonHeight);
+        }
+        [alertView addSubview:button];
+        [button addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+        
+    }
+
+}
+
+// alert背景点击手势，移除alert
+- (void)tapAction:(UITapGestureRecognizer *)tap {
+
+    [UIView animateWithDuration:.35
+                     animations:^{
+                         [tap.view viewWithTag:1001].transform = CGAffineTransformMakeTranslation(0, kScreenHeight*.3);
+                     } completion:^(BOOL finished) {
+                         [tap.view removeFromSuperview];
+                     }];
+    
+
+}
+
+// alert按钮响应
+- (void)buttonAction:(UIButton *)button {
+
+    NSInteger buttonTag = button.tag - 1500;
+    if (buttonTag == 0) {
+        NSLog(@"记录生活");
+    } else if (buttonTag == 1) {
+        NSLog(@"视频拍照");
+    } else if (buttonTag == 2) {
+        NSLog(@"从手机相册获取");
+    } else {
+        NSLog(@"取消");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ HomePage/FriendsCircleVC/MomentView.h
@@ -0,0 +1,15 @@
+//
+//  MomentView.h
+//  XiaoHuiBang
+//
+//  Created by mac on 16/11/7.
+//  Copyright © 2016年 消汇邦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MomentView : UIView
+
+
+
+@end
